d7c72e58afc211f1edf3f73a4f59e56751f95c2f
01Pratham <cprathmesh01@gmail.com>
2023-08-25 17:51:49 +0530

    commit

--- controller/discounting.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "Currency_Format.php";
-if (isset($_POST['type'])) {
-    $mrc = intval($_POST['cost']) * intval($_POST['qty']);
-    $perc = intval($_POST['percent']) / 100;
-    $disc = $mrc - ($mrc * $perc);
-    INR($disc);
-}
