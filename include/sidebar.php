209d6591ca586565a1c96afa4f480b76173fd4a6
01Pratham <cprathmesh01@gmail.com>
2023-08-25 17:54:11 +0530

    Commit

---
+++ include/sidebar.php
@@ -0,0 +1,60 @@
+<aside class="main-sidebar sidebar-light-primary elevation-4">
+    <a href="" class="brand-link mt-2">
+        <img src="../include/dist/img/logo.png" alt="ESDS Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Configurator</span>
+    </a>
+    <div class="except sidebar">
+        <div class="except user-panel mt-3 pb-2 mb-3 d-flex">
+            <div class="except image">
+                <img src="../include/dist/img/avatar.png" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="except info">
+                <input type="checkbox" id="profileHead" class="head-btn d-none" oninput="if($(this).prop('checked')){ $('#collapseExample').removeClass('hiddenDiv') ; $('#collapseExample').addClass('show')} else {$('#collapseExample').removeClass('show') ; $('#collapseExample').addClass('hiddenDiv')}">
+                <label class="text-left btn border-0" for="profileHead" id="profile_<?= $id ?>" style="z-index: 1;">
+                    <?= $get_emp['first_name'] . " " . $get_emp['last_name'] ?>
+                </label>
+                <div class="hiddenDiv except" id="collapseExample">
+                    <div class=" card-body except">
+                        <a href="../controller/logout.php"><span class="fas fa-sign-out-alt mr-2"> </span>Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link active">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p class="except">
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?all" class="nav-link" id="allEstms">
+                        <i class="nav-icon fa fa-folder-open"></i>
+                        <p class="except">
+                            Saved Quotations
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?create_new" class="nav-link" id="create">
+                        <i class="nav-icon fas fa-cogs"></i>
+                        <p class="except">
+                            Create New
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?users" class="nav-link" id="teamUsers">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p class="except">
+                            Team
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
---
+++ view/Components/DRServices.php
@@ -0,0 +1,53 @@
+<div class="contain-btn btn-link border-bottom DR_<?= $name ?> d-none">
+    <button class="btn btn-link col-md-11 text-left" data-toggle="collapse" type="button" role="button"
+        aria-expanded="true" aria-controls="dr_collapse<?= $id ?>">
+        <i class="fa fa-server mr-2"></i>Disaster Recovery Services :
+    </button>
+</div>
+<div class="collapse py-1 ml-3 show DR_<?= $name ?> d-none" id="dr_collapse_<?= $id ?>">
+    <div class="row">
+        <div class="form-group col-md-4 row my-3">
+            <select id="drm-select_<?= $id ?>" class="border-0 " style="width: 70%;">
+                <option value="">DRM tool</option>
+            </select>
+            <input type="checkbox" name="drm[<?= $name ?>]" id="drm_<?= $id ?>"
+                class="check <?= ($Editable['drm'][$name] == "on") ? "Checked" : "" ?>">
+            <!-- <input type="number" min = 0  id="drmqty_<?= $id ?>" name="drmqty[<?= $name ?>]" class="hide form-control"> -->
+        </div>
+        <div class="form-group col-md-4 row my-3">
+            <select id="dr_drill-select_<?= $id ?>" class="border-0 " style="width: 70%;">
+                <option value="">DR Drill</option>
+            </select>
+            <input type="checkbox" name="dr_drill[<?= $name ?>]" id="dr_drill_<?= $id ?>"
+                class="check <?= ($Editable['dr_drill'][$name] == "on") ? "Checked" : "" ?>">
+            <input type="number" min=0 id="drillqty_<?= $id ?>" name="drillqty[<?= $name ?>]" class="hide form-control"
+                value="<?= $Editable['drillqty'][$name] ?>" style="width: 75%" placeholder="Frequency">
+        </div>
+        <div class="form-group col-md-4 row my-3">
+            <select name="rep_link_type[<?= $name ?>]" id="rep_link_type-select_<?= $id ?>" class="border-0 "
+                style="width: 70%;">
+                <option class="editable" value="<?= $Editable['rep_link_type'][$name] ?>" hidden><?= $Editable['rep_link_type'][$name] ?></option>
+                <option value="">Replication Link</option>
+                <?php create_opt('link') ?>
+            </select>
+            <input type="checkbox" name="rep_link[<?= $name ?>]" id="rep_link_type_<?= $id ?>"
+                class="check <?= ($Editable['rep_link'][$name] == "on") ? "Checked" : "" ?>">
+            <!-- <input type="number" min=0 id="rep_link_qty_<?= $id ?>" name="rep_link_qty[<?= $name ?>]" class="hide form-control" value="<?= $Editable['rep_link_type'][$name] ?>" style="width: 75%" placeholder="Mbps"> -->
+            <div class="input-group hide" style="width: 75%">
+                <input type="number" min=0 id="rep_link_qty_<?= $id ?>" name="rep_link_qty[<?= $name ?>]"
+                    class="form-control  my-1" value="<?= $Editable['rep_link_type'][$name] ?>"
+                    aria-describedby="inputGroupPrepend">
+                <span class="input-group-text py-0 form-control my-1 bg-light col-3"
+                    id="ReplUnit_<?= $id ?>">Mbps</span>
+            </div>
+        </div>
+        <div class="form-group col-md-4 row my-3">
+            <select id="repmgmt-select_<?= $id ?>" class="border-0 " style="width: 70%;">
+                <option value="">Replication Link Management</option>
+            </select>
+            <input type="checkbox" name="rep_link_mgmt[<?= $name ?>]" id="rep_link_mgmt_<?= $id ?>"
+                class="check <?= ($Editable['rep_link_mgmt'][$name] == "on") ? "Checked" : "" ?>">
+            <!-- <input type="number" min = 0  id="drmqty_<?= $id ?>" name="drmqty[<?= $name ?>]" class="hide form-control"> -->
+        </div>
+    </div>
+</div>
